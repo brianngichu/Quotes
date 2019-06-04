80ab39b98844a8eb460acd91c229d07311125020
Ngichu <brianngichu@gmail.com>
2019-06-04 09:05:04 +0300

    andular quote generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
+import { TimeCountPipe } from './time-count.pipe';
+import { HighlightDirective } from './highlight.directive';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    TimeCountPipe,
+    HighlightDirective,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
