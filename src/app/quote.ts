4cefce7461ac270ac770652996c9d1a51e5286e7
Ngichu <brianngichu@gmail.com>
2019-06-06 10:09:09 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,4 +0,0 @@
- export class Quote {
-   constructor(public name:string, public quoteWords:string, public author:string, public completeDate:Date, public upVote:number, public downVote:number){
-   }
-}
